3549b808d1bf492755c47d094462fa21f15a98c1
Antonio Junior <juniordsj@gmail.com>
2020-11-11 20:29:38 -0300

    :sparkles: atarting-back-end GoBarber

---
+++ src/database/migrations/1605136475348-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1605136475348 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
